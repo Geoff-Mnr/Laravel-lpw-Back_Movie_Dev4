b32183b91c35b20dd9f7eb0f4c6ab81f1b7a069a
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-03-06 21:22:35 +0100

    update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,15 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->longText('description')->nullable();
+            $table->longText('comment')->nullable();
+            $table->string('status', 1)->default('N');
+            $table->boolean('is_active')->default(true);
+            $table->unsignedInteger('created_by')->nullable(false)->default(1);
+            $table->unsignedInteger('updated_by')->nullable(true);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
