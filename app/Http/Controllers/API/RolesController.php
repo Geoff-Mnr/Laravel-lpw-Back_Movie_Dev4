679e7500f9239f50ce876e031f469e6e6e0cbf1e
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-04-04 21:04:43 +0200

    update

--- app/Http/Controllers/API/RolesController.php
+++ app/Http/Controllers/API/RolesController.php
@@ -14,7 +14,12 @@ class RolesController extends BaseController
      */
     public function index()
     {
-        //
+        try {
+            $roles = Role::all();
+            return $this->handleResponseNoPagination('Roles retrieved successfully', $roles, 200);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(), 400);
+        }
     }
 
     /**
@@ -22,7 +27,18 @@ class RolesController extends BaseController
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+
+            ]);
+        
+            $role = Role::create($request->all());
+
+            return $this->handleResponseNoPagination('Role created successfully', $role, 200);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(), 400);
+        }
     }
 
     /**
@@ -30,7 +46,17 @@ class RolesController extends BaseController
      */
     public function show(Role $role)
     {
-        //
+        try{
+        $role = Role::where('id', $role->id)->with('users')->first();
+
+        if ($role) {
+            return $this->handleResponseNoPagination('Role retrieved successfully', $role, 200);
+        } else {
+            return $this->handleError('Role not found', 404);
+        } 
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(), 400);
+        }
     }
 
     /**
@@ -38,7 +64,17 @@ class RolesController extends BaseController
      */
     public function update(Request $request, Role $role)
     {
-        //
+        try {
+            $role= Role::find($role->id);
+            if ($role) {
+                $role->update($request->all());
+                return $this->handleResponseNoPagination('Role updated successfully', $role, 200);
+            } else {
+                return $this->handleError('Role not found', 404);
+            }
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(), 400);
+        }
     }
 
     /**
@@ -46,6 +82,16 @@ class RolesController extends BaseController
      */
     public function destroy(Role $role)
     {
-        //
+        try {
+            $role = Role::find($role->id);
+            if ($role) {
+                $role->delete();
+                return $this->handleResponseNoPagination('Role deleted successfully', $role, 200);
+            } else {
+                return $this->handleError('Role not found', 404);
+            }
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(), 400);
+        }
     }
 }
