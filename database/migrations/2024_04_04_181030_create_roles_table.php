6a4748938c4027ace6d495b846e9a0f8ddf52d0d
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-04-04 20:47:47 +0200

    Update 2024_04_04_181030_create_roles_table.php

--- database/migrations/2024_04_04_181030_create_roles_table.php
+++ database/migrations/2024_04_04_181030_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->longText('description')->nullable();
             $table->string('status',1)->default('N');
             $table->boolean('is_active')->default(true);
