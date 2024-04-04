cdd051bb9999481d1fea947d8b7e3cf9f2b634b1
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-04-04 20:25:24 +0200

    Create Role.php

---
+++ app/Models/Role.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Role extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'is_active',
+        'created_by',
+        'updated_by',
+    ];
+
+    // Define the relationship with the User model
+    public function users()
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
+}
