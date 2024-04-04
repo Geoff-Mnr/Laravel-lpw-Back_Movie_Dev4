3d85fecd57da9925c774271dca5a138343c0b802
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-04-04 20:48:40 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained('roles');
-            $table->string('name');
+            $table->foreignId('role_id')->constrained();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
